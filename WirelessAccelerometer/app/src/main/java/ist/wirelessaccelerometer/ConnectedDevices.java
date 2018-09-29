6d689ae6fe1803fba1f9f637f95315570ac35730
luke <wisemanl@msu.edu>
2018-09-29 15:35:04 -0400

    added button and dropdown menu response, updated a number of extensions and such

---
+++ WirelessAccelerometer/app/src/main/java/ist/wirelessaccelerometer/ConnectedDevices.java
@@ -0,0 +1,6 @@
+package ist.wirelessaccelerometer;
+
+//class to contain, and allow for the accessing of, a map of string names to device UUID's for bluetooth
+//connected sensor to the cell-phone app. TODO: implement
+public class ConnectedDevices {
+}
--- WirelessAccelerometer/app/src/main/java/ist/wirelessaccelerometer/MainActivity.java
+++ WirelessAccelerometer/app/src/main/java/ist/wirelessaccelerometer/MainActivity.java
@@ -1,22 +1,37 @@
 package ist.wirelessaccelerometer;
 
+import android.content.Context;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean debug = true; //flag to enable debug messages, enable for testing
+    private String logTag="log_placeholder";
+    private Spinner device_dropdown;
 
+    //Run on activity startup only, initialize button connections and threads.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        addItemSelectionListener(); //init dropdown device menu
+        BluetoothSearchButton(); //init bluetooth button
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +63,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //initialize dropdown menu callback, called when user selects an option
+    public void addItemSelectionListener() {
+        device_dropdown = (Spinner) findViewById(R.id.spinner_modes);
+        device_dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                //item selection handle here
+                if (debug) Log.d(logTag, "Dropdown item selected: " + parent.getItemAtPosition(pos).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+    //Initialize button press callback for the "Scan for devices" button
+    public void BluetoothSearchButton() {
+        Button bluetooth_search = (Button) findViewById(R.id.bluetooth_scan_button);
+        bluetooth_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (debug) Log.d(logTag,"searching for bluetooth devices...");
+            }
+        });
     }
 }
