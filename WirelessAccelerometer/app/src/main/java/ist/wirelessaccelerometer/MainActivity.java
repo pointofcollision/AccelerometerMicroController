ebb7e66fb092e3d23a5454cf0e0a749fed7aac38
luke <wisemanl@msu.edu>
2018-10-23 09:50:57 -0400

    adds bluetooth detection, without active streaming.

--- WirelessAccelerometer/app/src/main/java/ist/wirelessaccelerometer/MainActivity.java
+++ WirelessAccelerometer/app/src/main/java/ist/wirelessaccelerometer/MainActivity.java
@@ -1,6 +1,8 @@
 package ist.wirelessaccelerometer;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,6 +10,9 @@ import android.content.IntentFilter;
 import android.hardware.usb.UsbManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ParcelUuid;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.transition.Visibility;
@@ -28,16 +33,34 @@ import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity implements ConfigMenu.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
     public static boolean debug = true; //flag to enable debug messages, enable for testing
     private String logTag="log_placeholder";
     private Spinner device_dropdown;
     public final static String ACTION_CONFIG="ist.wirelessaccelerometer.ACTION_CONFIG"; //intent tag
     BroadcastReceiver fragmentReceiver;
     ConnectedDevices connDevices;
+    private BluetoothAdapter mBTAdapter;
+    private Set<BluetoothDevice> mPairedDevices;
+    private Handler mHandler; // Our main handler that will receive callback notifications
+    private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
+    private BluetoothSocket mBTSocket = null; // bi-directional client-to-client data path
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
+    // #defines for identifying shared types between calling functions
+    private final static int REQUEST_ENABLE_BT = 1; // used to identify adding bluetooth names
+    private final static int MESSAGE_READ = 2; // used in bluetooth handler to identify message update
+    private final static int CONNECTING_STATUS = 3; // used in bluetooth handler to identify message status
+
+
     private int sensorCount = 1; //used for new sensor ID naming, unless they rename it
     //Run on activity startup only, initialize button connections and threads.
     @Override
@@ -48,19 +71,16 @@ public class MainActivity extends AppCompatActivity implements ConfigMenu.OnFrag
         setSupportActionBar(toolbar);
 
         addItemSelectionListener(); //init dropdown device menu
-        BluetoothSearchButton(); //init bluetooth button
         ConfigButton(); //init config button
         StartButton();
         StopButton();
+        BluetoothScanButton();
 
 
-        int REQUEST_ENABLE_BT = 1; //"locally defined integer greater than 0" according to documentation
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (!mBluetoothAdapter.isEnabled()) {  //
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
 
+        mBTAdapter = BluetoothAdapter.getDefaultAdapter();
+        /*START BLUETOOTH SETUP */
+        bluetoothOn(getCurrentFocus());
         connDevices = new ConnectedDevices();
         //testing purposes only
         String testUUID = "New sensor 1";
@@ -68,6 +88,27 @@ public class MainActivity extends AppCompatActivity implements ConfigMenu.OnFrag
         connDevices.addSensor(testUUID,testSensor);
         addDropdownSensor(testUUID);
         //testing purposes only
+        mHandler = new Handler(){
+            public void handleMessage(android.os.Message msg){
+                if(msg.what == MESSAGE_READ){
+                    String readMessage = null;
+                    try {
+                        readMessage = new String((byte[]) msg.obj, "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                if(msg.what == CONNECTING_STATUS){
+                    if(msg.arg1 == 1)
+                        if (debug) Log.d(logTag, "connection_something message");
+                    else
+                        if (debug) Log.d(logTag, "other type of comm message");
+                }
+            }
+        };
+        //END BLUETOOTH SETUP
     }
 
     /*
@@ -142,9 +183,37 @@ public class MainActivity extends AppCompatActivity implements ConfigMenu.OnFrag
 
         return super.onOptionsItemSelected(item);
     }
-    public void onFragmentInteraction(Uri uri){
-        android.support.v4.app.Fragment fragment = getSupportFragmentManager().findFragmentByTag("fragmentID");
-    };
+    //public void onFragmentInteraction(Uri uri){
+    //    android.support.v4.app.Fragment fragment = getSupportFragmentManager().findFragmentByTag("fragmentID");
+    //};
+
+    /*
+    Function to list any paired bluetooth devices, makes toast currently
+     */
+    private void listPairedDevices(View view){
+        mPairedDevices = mBTAdapter.getBondedDevices();
+        if(mBTAdapter.isEnabled()) {
+            // put it's one to the adapter
+            for (BluetoothDevice device : mPairedDevices) {
+                if (debug) Log.d(logTag, "Device name: " + device.getName());
+                if (debug) Log.d(logTag, "Device address: " + device.getAddress());
+
+                try {
+                    ParcelUuid[] deviceUUIDs = device.getUuids();
+                    if (debug) Log.d(logTag, "Device UUID size: " + String.valueOf(device.getUuids().length));
+                    if (debug) Log.d(logTag, "Device UUID: " + deviceUUIDs[0].toString());
+                } catch (NullPointerException e) {
+                    if (debug) Log.d(logTag, "Null device UUID array");
+                }
+
+            }
+
+            Toast.makeText(getApplicationContext(), "Show Paired Devices", Toast.LENGTH_SHORT).show();
+        }
+        else
+            Toast.makeText(getApplicationContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
+    }
+
     //initialize dropdown menu callback, called when user selects an option
     public void addItemSelectionListener() {
         device_dropdown = (Spinner) findViewById(R.id.spinner_modes);
@@ -171,16 +240,6 @@ public class MainActivity extends AppCompatActivity implements ConfigMenu.OnFrag
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-    }
-    //Initialize button press callback for the "Scan for devices" button
-    public void BluetoothSearchButton() {
-        Button bluetooth_search = (Button) findViewById(R.id.bluetooth_scan_button);
-        bluetooth_search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (debug) Log.d(logTag,"searching for bluetooth devices...");
             }
         });
     }
@@ -258,6 +317,18 @@ public class MainActivity extends AppCompatActivity implements ConfigMenu.OnFrag
         //this will add one value to the values currently stored on the dropdown menu
     }
 
+    private void bluetoothOn(View view){
+        if (!mBTAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            Toast.makeText(getApplicationContext(),"Bluetooth turned on",Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Bluetooth is already on", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     /*
     Function to change the old UUID on the dropdown menu to the new one
      */
@@ -303,7 +374,42 @@ public class MainActivity extends AppCompatActivity implements ConfigMenu.OnFrag
 
     }
 
+    /*
+    Function to discover attached bluetooth devices
+     */
+    private void discover(View view){
+        // Check if the device is already discovering
+        if (debug) Log.d(logTag,"discover launched");
+        listPairedDevices(view);
+        if(mBTAdapter.isDiscovering()){
+            mBTAdapter.cancelDiscovery();
+            Toast.makeText(getApplicationContext(),"Discovery stopped",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            if(mBTAdapter.isEnabled()) {
 
+                mBTAdapter.startDiscovery();
+                Toast.makeText(getApplicationContext(), "Discovery started", Toast.LENGTH_SHORT).show();
+                registerReceiver(blReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
+                bluetoothOn(view);
+            }
+        }
+    }
+
+    final BroadcastReceiver blReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // add the name to the list
+                if (debug) Log.d(logTag,"device detected: " + device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
 
 
     //Initialize press callback for "Configure device" button
@@ -321,5 +427,85 @@ public class MainActivity extends AppCompatActivity implements ConfigMenu.OnFrag
                 editNameDialogFragment.show(fm, "fragment_config_menu_dialog");
             }
         });
+    }
+
+
+    //Initialize press callback for "Scan for Devices" button
+    private void BluetoothScanButton() {
+        Button config_btn = (Button) findViewById(R.id.bluetooth_scan_button);
+        config_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (debug) Log.d(logTag,"scanning for devices pressed");
+                discover(v);
+            }
+        });
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        return  device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        //creates secure outgoing connection with BT device using UUID
+    }
+
+    /*class to handle the connection thread, outside of the UI
+    * credit: http://mcuhq.com/27/simple-android-bluetooth-application-with-arduino-example*/
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.available();
+                    if(bytes != 0) {
+                        SystemClock.sleep(100); //pause and wait for rest of data. Adjust this depending on your sending speed.
+                        bytes = mmInStream.available(); // how many bytes are ready to be read?
+                        bytes = mmInStream.read(buffer, 0, bytes); // record how many bytes we actually read
+                        mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                                .sendToTarget(); // Send the obtained bytes to the UI activity
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(String input) {
+            byte[] bytes = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) { }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
     }
 }
